522586a45e62dbaa9b8cc525587ad46102ec898c
Ben Abraham <babra023@SCSs-MacBook-Pro.local>
2025-06-13 17:31:41 -0400

    updates

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -28,54 +28,53 @@ export const EXPERIENCES = [
     year: "",
     role: "Real Time Crime Center Analyst",
     company: "New Jersey State Police",
-    description: `Data monitoring and analysis, Technological support, Reporting and
-    documentation`,
-    technologies: ["Law Enforcment Databases" ,"ALPRs", "365 Suite"]
+    description: `Monitored data streams and conducted real time analysis to support law enforcement operations. 
+    Provided technological support, prepared analytical reports, and worked with law enforcement databases, open-source 
+    intelligence (OSINT), and automated license plate readers (ALPRs).`,
+    technologies: [,"OSINT", "ALPRs", "Law Enforcment Data Monotiring", "MS 365 Suite"]
   },
   {
     year: "",
-    role: "NSF Build and Broaden Research Fellowship",
+    role: "NSF Data Science Researcher",
     company: "Kean University",
-    description: `Assisted in the development and implementation of machine learning
-    models to predict contingent workers using logistic regression and
-    other statistical techniques.`,
-    technologies: ["Python", "STATA", "365 Suite"]
+    description: `Assisted in developing machine learning models to predict contingent labor trends using logistic 
+    regression and statistical analysis. Supported data cleaning, visualization, and interpretation for academic research deliverables.`,
+    technologies: ["Python", "STATA", "Data Visualization","MS 365 Suite"]
   },
   {
     year: "",
-    role: "End User Field Services Technician Intern",
+    role: "End User Field Services Technician",
     company: "NJ Transit",
-    description: `Hardware and Software installation and maintenance, User Assistance,
-    Security Compliance and Documentation.`,
-    technologies: ["Active Directory", "TCP/IP", "PowerShell", "365 Suite"]
+    description: `Installed and maintained hardware/software for field offices. Provided user support, enforced security compliance standards, 
+    and maintained system documentation. Used Active Directory and PowerShell for diagnostics and user account management.`,
+    technologies: ["Active Directory", "TCP/IP", "PowerShell", "MS 365 Suite"]
   },
   {
     year: "",
-    role: "CAHSI Research Scholar",
+    role: "CAHSI Cyber Security Researcher",
     company: "University of Texas, Kean University",
     description: `Conducted mentored research as part of the CAHSI REU program, and
     engaged in training to strengthen my computing, communication, and
     professional skills. Created a research plan, maintained a journal to
     report on research progress, and created a research poster to
     disseminate my research results..`,
-    technologies: ["Python", "365 Suite"]
+    technologies: ["Python", "MS 365 Suite"]
   },
   {
     year: "",
-    role: "FIU Research Intern",
+    role: "FIU Research Assistant",
     company: "Florida International University",
-    description: `Contributed and assisted in the development of a deep learning-based
-    radio fingerprinting system for IoT device authentication.`,
-    technologies: ["Python", "SDR"]
+    description: `Contributed to the development of a deep learning-based radio fingerprinting system for IoT device authentication. Gained experience with 
+    software-defined radios (SDR), RF signal processing, and device-level security modeling.`,
+    technologies: ["Python", "IoT","SDR", "Recivers and Transmittors"]
   },
   {
     year: "",
     role: "Cisco Network Intern",
     company: "Cisco inc.",
-    description: `Worked in a team consisted of Round Rock and Cisco network
-    engineers to set up all WIFI-6 access points for the Global Citizens
-    Festival in NYC.`,
-    technologies: ["TCP/IP"]
+    description: `Collaborated with Cisco and Round Rock engineers to deploy Wi-Fi 6 access points for the Global Citizens Festival in NYC. 
+    Gained practical experience in large-scale event network setup and wireless infrastructure optimization.`,
+    technologies: ["TCP/IP", "Network Deployment", "Wireless Infrastructure", "Wi-Fi 6"]
   },
  
 ];
